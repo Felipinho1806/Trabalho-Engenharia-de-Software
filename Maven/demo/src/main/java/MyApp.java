26d9afbacb4d0d5bf894a53b228d1890daa35175
Felipinho1806 <101186686+Felipinho1806@users.noreply.github.com>
2025-01-06 00:58:06 -0300

    Add files via upload

---
+++ Maven/demo/src/main/java/MyApp.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class MyApp {
+    public static void main(String[] args) {
+     
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Já possui cadastro? (S/N)");
+        String resposta = sc.nextLine();
+
+        if(resposta.toLowerCase().equals("n")) {
+            InsertBd.main(args);
+        }
+        else {
+            Conexao.main(args);
+        }
+    }
+}
